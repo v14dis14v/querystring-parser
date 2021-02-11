e7aeb4e6ad1f7b8b2608238f81657c9dc1bb96a4
v14dis14v <vladislav.reshetow@yandex.ru>
2021-02-11 14:12:35 +0300

    Исправляет сбор querystring

--- querystring_parser/builder.py
+++ querystring_parser/builder.py
@@ -7,7 +7,6 @@ Created on 2012-03-28
 Updated 2012-04-01 Bernard 'berni' Kobos
 '''
 
-
 try:
     # for Python3
     import urllib.parse as urllib
@@ -23,35 +22,36 @@ except NameError:
 
 
 def build(item, encoding=None):
-	def recursion(item, base=None):
-		pairs = list()
-		if(hasattr(item, 'values')):
-			for key, value in item.items():
-				if encoding:
-					quoted_key = urllib.quote(unicode(key).encode(encoding))
-				else:
-					quoted_key = urllib.quote(unicode(key))
-				if(base):
-					new_base = "%s[%s]" % (base, quoted_key)
-					pairs += recursion(value, new_base)
-				else:
-					new_base = quoted_key
-					pairs += recursion(value, new_base)
-		elif(isinstance(item, list)):
-			for (index, value) in enumerate(item):
-				if(base):
-					new_base = "%s" % (base)
-					pairs += recursion(value, new_base)
-				else:
-					pairs += recursion(value)
-		else:
-			if encoding:
-				quoted_item = urllib.quote(unicode(item).encode(encoding))
-			else:
-				quoted_item = urllib.quote(unicode(item))
-			if(base):
-				pairs.append("%s=%s" % (base, quoted_item))
-			else:
-				pairs.append(quoted_item)
-		return pairs
-	return '&'.join(recursion(item))
+    def recursion(item, base=None):
+        pairs = list()
+        if (hasattr(item, 'values')):
+            for key, value in item.items():
+                if encoding:
+                    quoted_key = urllib.quote(unicode(key).encode(encoding))
+                else:
+                    quoted_key = urllib.quote(unicode(key))
+                if (base):
+                    new_base = "%s[%s]" % (base, quoted_key)
+                    pairs += recursion(value, new_base)
+                else:
+                    new_base = quoted_key
+                    pairs += recursion(value, new_base)
+        elif (isinstance(item, list)):
+            for (index, value) in enumerate(item):
+                if (base):
+                    new_base = "%s[%s]" % (base, index)
+                    pairs += recursion(value, new_base)
+                else:
+                    pairs += recursion(value)
+        else:
+            if encoding:
+                quoted_item = urllib.quote(unicode(item).encode(encoding))
+            else:
+                quoted_item = urllib.quote(unicode(item))
+            if (base):
+                pairs.append("%s=%s" % (base, quoted_item))
+            else:
+                pairs.append(quoted_item)
+        return pairs
+
+    return '&'.join(recursion(item))
